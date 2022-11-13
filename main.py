86b2e53ec5c63affef872699a2d58e197bc9fbe4
Paul-Jaulin <paul.jaulin.ai@gmail.com>
2022-11-13 22:59:18 +0100

    re

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import pip
-pip.main(['install','discord.py'])
 import discord
-import sys
-import os
-sys.path.append(os.getcwd())
-#from FinalProject.Data_Handling_tools.visualize import football
-from Data_Handling_tools.visualize2 import football
+import pandas as pd
+import plotly.express as plt
+import plotly.graph_objects as go
+
+APIkey = "19e55afb-07fc-4b68-b3b8-74c7950f0aee"'''
+df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude', 'AQI'])'''
+
 
 
 ##Personal information of the bit
@@ -13,7 +13,9 @@ TOKEN = 'MTAzNDc2MTQ4NDMwNTE4Njg3Ng.GQt7gk.RexGUYAlgVVKBjbeEcvuglQUOLrWX9-iA5KVp
 ##Discord channel also know as guild
 GUILD = 'PGE4 ADAV 2022-23'
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 ##To know about the bot id, discord channel and bot name
 @client.event
@@ -25,7 +27,7 @@ async def on_ready():
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})')
-
+'''
 ##Provide response to users request
 @client.event
 async def on_message(message):
@@ -74,3 +76,4 @@ async def on_message(message):
 
 client.run(TOKEN)
 
+'''
