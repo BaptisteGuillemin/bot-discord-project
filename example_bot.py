feeca87e2b3b3f02ba40fc0d376edd76c05f416e
Paul-Jaulin <paul.jaulin.ai@gmail.com>
2022-11-14 18:25:08 +0100

    while test

--- example_bot.py
+++ example_bot.py
@@ -19,17 +19,20 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send(f'Hello!! {message.author.name}')
         await message.channel.send('Did you want to check and plot on a map the air quality somewhere ? Yes or No.')
+        await message.channel.send("--------")
 #Etape 2 (yes or no)
     if message.content.lower() == 'no':
         await message.channel.send(f'Okay {message.author.name}, let me know when I will be able to help you, hope to see you soon !')
+        await message.channel.send("--------")
     if message.content.lower() == 'yes':
         await message.channel.send('Here are the countries where I can provide you the Air Quality Index')
         await message.channel.send('function get All country')
-        await message.channel.send("--------")
+
         await message.channel.send('In which country are you interested to know the current Air Quality Index ?')
         await message.channel.send("Bot developpement in progress...")
-'''
 
+
+'''
 user_answer = input("Did you want to check and plot on a map the air quality somewhere ? yes or no")
 while True:
   count=0
