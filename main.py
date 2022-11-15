2e7cd7e86bd71288ebe45b6f8b06a95647c84d95
Paul-Jaulin <paul.jaulin.ai@gmail.com>
2022-11-15 16:49:13 +0100

    re

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import discord
 import pandas as pd
-import airvisual_api_project.py as API_Visu
+import discord
+import requests
+import plotly.express as plt
+import plotly.graph_objects as go
 
-APIkey = "19e55afb-07fc-4b68-b3b8-74c7950f0aee"
-df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude', 'AQI'])
-
+import airvisual_api_project as API_Visu
 
 
 ## Bot informations 
-TOKEN = 'MTAzNDc2MTQ4NDMwNTE4Njg3Ng.GsaN-j.kBmI7RH-Pz6P5YPFheKsb7RIEd7g0pL9Rb6_uk'
+TOKEN = 'MTAzNDc2MTQ4NDMwNTE4Njg3Ng.GbTx7f.tS4ufSJ82oNMFXziItiK-9ommAqR9T2nDDu3bk'
 ##Discord channel also know as guild
 GUILD = 'PGE4 ADAV 2022-23'
 
@@ -22,13 +22,13 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 @client.event
 async def on_message(message):
-    if message.content.startswith('$Call_Bot' or 'Call'):
+    if message.content.startswith('$Call_Bot'):
         channel = message.channel
         await message.channel.send('I Can plot on a map the air quality or a city that we will choose !')
         await message.channel.send("--------")
 
         #Select the country
-        API_Visu.get_all_countries()
+        list_countries = API_Visu.get_all_countries()
         await message.channel.send('Here are the countries where I can provide you the Air Quality Index')
         await message.channel.send(list_countries)
 
@@ -39,9 +39,10 @@ async def on_message(message):
         Selected_country = msg_country.content
         await message.channel.send('Here is the country selected : ' + Selected_country)
         await message.channel.send("--------")
-        API_Visu.get_state(Selected_country)
+        
 
         #Select the state
+        states = API_Visu.get_state(Selected_country)
         await message.channel.send('After that I will need the State where you want to see the AQI')
         await message.channel.send('Here is the list of the States that you can choose')
         await message.channel.send(states)
@@ -53,15 +54,15 @@ async def on_message(message):
         Selected_state = msg_state.content
         await message.channel.send('Here is the state selected : ' + Selected_state)
         await message.channel.send("--------")
-        API_Visu.get_city(Selected_state, Selected_country)
         
         #Select the city
+        List_cities = API_Visu.get_city(Selected_state, Selected_country)
         await message.channel.send('After that I will need the city where you want to see the AQI')
         await message.channel.send('Here is the list of the cities that you can choose')
         await message.channel.send(List_cities)
 
         def check_city(m):
-            return m.content in List_citys and m.channel == channel
+            return m.content in List_cities and m.channel == channel
         
         msg_city = await client.wait_for('message', check=check_country)
         Selected_city = msg_city.content
