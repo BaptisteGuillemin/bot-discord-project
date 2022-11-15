403923daa3e007875cdf6fc1b74449d91810e267
Paul-Jaulin <paul.jaulin.ai@gmail.com>
2022-11-15 15:19:13 +0100

    main màj

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import discord
 import pandas as pd
-import plotly.express as plt
-import plotly.graph_objects as go
 
 APIkey = "19e55afb-07fc-4b68-b3b8-74c7950f0aee"
 df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude', 'AQI'])
@@ -9,7 +7,7 @@ df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude'
 
 
 ## Bot informations 
-TOKEN = 'MTAzNDc2MTQ4NDMwNTE4Njg3Ng.G_TVT4.UsDstUP1bUC3SoQdqmcUkmxOF66z9ERFcuk_wQ'
+TOKEN = 'MTAzNDc2MTQ4NDMwNTE4Njg3Ng.GsaN-j.kBmI7RH-Pz6P5YPFheKsb7RIEd7g0pL9Rb6_uk'
 ##Discord channel also know as guild
 GUILD = 'PGE4 ADAV 2022-23'
 
@@ -17,15 +15,28 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-##To know about the bot id, discord channel and bot name
+List_countries = ['France','Spain']
+
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'We have logged in as {client.user}')
+@client.event
+async def on_message(message):
+    if message.content.startswith('$countries'):
+        channel = message.channel
+        await message.channel.send('Here are the countries where I can provide you the Air Quality Index')
+        await message.channel.send(List_countries)
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})')
+        def check_country(m):
+            return m.content in List_countries and m.channel == channel
 
+        msg = await client.wait_for('message', check=check_country)
+        Selected_country = msg.content
+        await message.channel.send('Here is the country selected : ' + Selected_country)
+        await message.channel.send('After that I will need the State where you want to see the AQI')
+        await message.channel.send('Here is the list of the States that you can choose')
+        await message.channel.send(List_states)
+
+        def check_State(m):
+            return m.content in List_countries and m.channel == channel
 client.run(TOKEN)
