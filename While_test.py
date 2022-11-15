121276ff4fd8795826301d02434fef8b936cd373
Paul-Jaulin <paul.jaulin.ai@gmail.com>
2022-11-15 14:49:46 +0100

    wait_gor

--- While_test.py
+++ While_test.py
@@ -3,7 +3,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('hello'):
-        While ETAPE1 == 'Vide':
+        While == 'Vide':
             await message.channel.send(f'Hello!! {message.author.name}')
             await message.channel.send('Did you want to check and plot on a map the air quality somewhere ? Yes or No.')
             await message.channel.send("--------")
