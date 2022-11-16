9b5afa49b99049777043dc50affd208d73953eeb
Baptiste Guilllemin <baptiste.guillemin@aivancity.education>
2022-11-16 11:51:05 +0100

    visu sur main

--- airvisual_api_project.py
+++ airvisual_api_project.py
@@ -18,22 +18,28 @@ df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude'
 # Get list of all countries available
 
 def get_all_countries():
-    url = "http://api.airvisual.com/v2/countries?key=19e55afb-07fc-4b68-b3b8-74c7950f0aee"
-    dic = requests.get(url).json()
-    dic = dic['data']
-    list_countries = [n['country'] for n in dic]
-    return list_countries
+    try:
+        url = "http://api.airvisual.com/v2/countries?key=19e55afb-07fc-4b68-b3b8-74c7950f0aee"
+        dic = requests.get(url).json()
+        dic = dic['data']
+        countries = [n['country'] for n in dic]
+        return countries
+    except:
+        return print("erreur avec l'API ")
 
 #get_all_countries()
 
 # Get list of states: return dictionnary of the country parameter as key value and list of states as value
 
 def get_state(country):
-    url = 'http://api.airvisual.com/v2/states?country={}&key={}'.format(country, APIkey)
-    dic =  requests.get(url).json()
-    dic = dic['data']
-    states = [n['state'] for n in dic]
-    return states
+    try:
+        url = 'http://api.airvisual.com/v2/states?country={}&key={}'.format(country, APIkey)
+        dic = requests.get(url).json()
+        dic = dic['data']
+        states = [n['state'] for n in dic]
+        return states
+    except:
+        return print("erreur avec l'API, trop de requête ")
 
 #get_state("India")
 
@@ -41,12 +47,14 @@ def get_state(country):
 # return dictionnary: key: state / value: list of cities
 
 def get_city(state, country):
-    city = {}
-    url = "http://api.airvisual.com/v2/cities?state={}&country={}&key={}".format(state, country, APIkey)
-    dic = requests.get(url).json()
-    dic = dic['data']
-    city[state] = [n['city'] for n in dic]
-    return city
+    try:
+        url = "http://api.airvisual.com/v2/cities?state={}&country={}&key={}".format(state, country, APIkey)
+        dic = requests.get(url).json()
+        dic = dic['data']
+        city = [n['city'] for n in dic]
+        return city
+    except:
+        return print("erreur avec l'API ")
 
 #get_city('Delhi', 'India')
 
@@ -54,42 +62,46 @@ def get_city(state, country):
 # return df
 
 def get_aqius(city, state, country):
-    df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude', 'AQI'])
-    dic = {}
-    URL = 'http://api.airvisual.com/v2/city?city={}&state={}&country={}&key={}'.format(city, state, country, APIkey)
-    data = requests.get(URL).json()
+    try:
+        df = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude', 'AQI'])
+        dic = {}
+        URL = 'http://api.airvisual.com/v2/city?city={}&state={}&country={}&key={}'.format(city, state, country, APIkey)
+        data = requests.get(URL).json()
 
-    dic['country']= country
-    dic['state']= state 
-    dic['city']= city
-    dic['latitude'] = data['data']['location']['coordinates'][0]
-    dic['longitude']= data['data']['location']['coordinates'][1]
-    dic['AQI']= data['data']['current']['pollution']['aqius']
+        dic['country']= country
+        dic['state']= state
+        dic['city']= city
+        dic['latitude'] = data['data']['location']['coordinates'][0]
+        dic['longitude']= data['data']['location']['coordinates'][1]
+        dic['AQI']= data['data']['current']['pollution']['aqius']
 
-    df = df.append(dic, ignore_index=True)
-    df['AQI'] = pd.to_numeric(df["AQI"])
+        df = df.append(dic, ignore_index=True)
+        df['AQI'] = pd.to_numeric(df["AQI"])
 
-    range_1 = range(0, 50)
-    if dic['AQI'] in range_1: df['AQI label'] = "good"
-    range_2 = range(50, 100)
-    if dic['AQI'] in range_2: df['AQI label'] = "Moderate"
-    range_3 = range(100, 150)
-    if dic['AQI'] in range_3: df['AQI label'] = "Unhealthy for sensitive groups"
-    range_4 = range(150, 200)
-    if dic['AQI'] in range_4: df['AQI label'] = "Unhealthy" 
-    range_5 = range(200, 300)
-    if dic['AQI'] in range_5: df['AQI label'] = "Very Unhealthy"
-    range_6 = range(300, 500)
-    if dic['AQI'] in range_6: df['AQI label'] = "Hazardous"
+        range_1 = range(0, 50)
+        if dic['AQI'] in range_1: df['AQI label'] = "good"
+        range_2 = range(50, 100)
+        if dic['AQI'] in range_2: df['AQI label'] = "Moderate"
+        range_3 = range(100, 150)
+        if dic['AQI'] in range_3: df['AQI label'] = "Unhealthy for sensitive groups"
+        range_4 = range(150, 200)
+        if dic['AQI'] in range_4: df['AQI label'] = "Unhealthy"
+        range_5 = range(200, 300)
+        if dic['AQI'] in range_5: df['AQI label'] = "Very Unhealthy"
+        range_6 = range(300, 500)
+        if dic['AQI'] in range_6: df['AQI label'] = "Hazardous"
 
-    return df
+        return df
+
+    except:
+        return print("erreur avec l'API ")
 
 """## Visualization functions
 
 
 """
 
-def map_df():
+def map_df(df):
     color_scale = [(0, 'lime'), (0.2, 'green'), (0.3, 'yellow'), (0.4, 'orange'), (0.6, 'red'), (1,'black')]
     range_color = [1, 500]
 
@@ -101,7 +113,7 @@ def map_df():
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig.show()
 
-#map_df()
+#map_df(df)
 
 def get_labels():
     colorscale = ['lime','green','yellow','orange','red','black']
