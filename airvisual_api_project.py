b2c832e30c8a4a5c51ab2dd2e78bdb1752eb3c36
Baptiste Guilllemin <baptiste.guillemin@aivancity.education>
2022-11-30 09:26:39 +0100

    zoom

--- airvisual_api_project.py
+++ airvisual_api_project.py
@@ -112,7 +112,7 @@ def map_df(df):
 
     fig = plt.scatter_mapbox(df, lat="longitude", lon="latitude", hover_name="city", hover_data=["city", "AQI"], opacity=1,
                             color="AQI", color_continuous_scale= color_scale, range_color=range_color,
-                            size='AQI', size_max=10, zoom=6, height=400, width=600)
+                            size='AQI', size_max=10, zoom=8, height=400, width=600)
 
     fig.update_layout(mapbox_style='carto-positron')
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
@@ -151,95 +151,9 @@ def get_labels():
     fig.update_layout(height=180,paper_bgcolor='LightSteelBlue',margin=dict(l=5,r=5,t=5,b=5))
     return pio.write_image(fig, "table.png")
 
-#get_labels()
 
-# get automatic zoom for the map, in case of multiple plot with far distance
 
-def zoom_center(lons: tuple=None, lats: tuple=None, lonlats: tuple=None,
-        format: str='lonlat', projection: str='mercator',
-        width_to_height: float=2.0) -> (float, dict):
-    """Finds optimal zoom and centering for a plotly mapbox.
-    Must be passed (lons & lats) or lonlats.
-    Parameters
-    --------
-    lons: tuple, optional, longitude component of each location
-    lats: tuple, optional, latitude component of each location
-    lonlats: tuple, optional, gps locations
-    format: str, specifying the order of longitud and latitude dimensions,
-        expected values: 'lonlat' or 'latlon', only used if passed lonlats
-    projection: str, only accepting 'mercator' at the moment,
-        raises `NotImplementedError` if other is passed
-    width_to_height: float, expected ratio of final graph's with to height,
-        used to select the constrained axis.
-    Returns
-    --------
-    zoom: float, from 1 to 20
-    center: dict, gps position with 'lon' and 'lat' keys
 
-    >>> print(zoom_center((-109.031387, -103.385460),
-    ...     (25.587101, 31.784620)))
-    (5.75, {'lon': -106.208423, 'lat': 28.685861})
-    """
-    if lons is None and lats is None:
-        if isinstance(lonlats, tuple):
-            lons, lats = zip(*lonlats)
-        else:
-            raise ValueError(
-                'Must pass lons & lats or lonlats'
-            )
-    
-    maxlon, minlon = max(lons), min(lons)
-    maxlat, minlat = max(lats), min(lats)
-    center = {
-        'lon': round((maxlon + minlon) / 2, 6),
-        'lat': round((maxlat + minlat) / 2, 6)
-    }
-    
-    # longitudinal range by zoom level (20 to 1)
-    # in degrees, if centered at equator
-    lon_zoom_range = np.array([
-        0.0007, 0.0014, 0.003, 0.006, 0.012, 0.024, 0.048, 0.096,
-        0.192, 0.3712, 0.768, 1.536, 3.072, 6.144, 11.8784, 23.7568,
-        47.5136, 98.304, 190.0544, 360.0
-    ])
-    
-    if projection == 'mercator':
-        margin = 1.2
-        height = (maxlat - minlat) * margin * width_to_height
-        width = (maxlon - minlon) * margin
-        lon_zoom = np.interp(width , lon_zoom_range, range(20, 0, -1))
-        lat_zoom = np.interp(height, lon_zoom_range, range(20, 0, -1))
-        zoom = round(min(lon_zoom, lat_zoom), 2)
-    else:
-        raise NotImplementedError(
-            f'{projection} projection is not implemented'
-        )
-    
-    return zoom, center
-
-'''user_answer = input("Did you want to check and plot on a map the air quality somewhere ? yes or no")
-if user_answer.lower() == 'no': print("Ok, an another time")
-else: 
-    countries = get_all_countries()
-    print("Select a country by copy and past")
-
-    while True:
-      print(countries)
-      country = input('In which country are you interested to know the current AQI ?')
-      if country == "no": break
-      else: print(get_state(country)) 
-
-      state = input('In which state are you interested to know the current AQI ?')
-      print(get_city(state, country))
-
-      city = input('In which city are you interested to know the current AQI ?')
-      DF = DF.append(get_aqius(city, state, country))  
-      # print visualisation
-      map_df()
-      get_labels()
-
-      loop = input("Would like to plot another city's AQI on the map ? yes or no")
-      if loop == "no": break'''
 
 def get_default_visu(city,state,country):
     DF = pd.DataFrame(columns = ['country', 'state', 'city', 'latitude', 'longitude', 'AQI'])
